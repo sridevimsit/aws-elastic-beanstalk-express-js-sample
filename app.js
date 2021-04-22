013c5c82b725afaa793461e4feda8b3e78ddbf28
sridevimsit <sridevi@msitprogram.net>
2021-04-22 16:38:10 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Welcome to the MSIT'));
+app.get('/', (req, res) => res.send('Hello Welcome to the MSIT\n\n new pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
